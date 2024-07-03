b4f31bd7473ecc4739f7c67210f988a99a41522b
靈夢辛普 <146130238+ReimuSimpu@users.noreply.github.com>
2024-07-03 16:21:24 -0500

    Update ServerHop.lua

--- ServerHop.lua
+++ ServerHop.lua
@@ -1,16 +1,16 @@
 local HttpService = game:GetService("HttpService")
 
 local JobIDs = {}
-    repeat
-        task.wait(0.1)
-        local API = HttpService:JSONDecode(
-            game:HttpGet("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Desc&limit=100" .. (ServerPages and "&cursor=" .. ServerPages or ""))
-        )
-        for _, v in ipairs(API["data"]) do
-            if v["id"] ~= game.JobId and v["playing"] ~= v["maxPlayers"] then
-                table.insert(JobIDs, v["id"])
-            end
+repeat
+    task.wait(0.1)
+    local API = HttpService:JSONDecode(
+        game:HttpGet("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Desc&limit=100" .. (ServerPages and "&cursor=" .. ServerPages or ""))
+    )
+    for _, v in ipairs(API["data"]) do
+        if v["id"] ~= game.JobId and v["playing"] ~= v["maxPlayers"] then
+            table.insert(JobIDs, v["id"])
         end
-        ServerPages = API.nextPageCursor
-    until #JobIDs >= 100
-    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, JobIDs[math.random(1, #JobIDs)])
+    end
+    ServerPages = API.nextPageCursor
+until #JobIDs >= 100
+game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, JobIDs[math.random(1, #JobIDs)])
