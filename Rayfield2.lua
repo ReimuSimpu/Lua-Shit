53212b3bfb0aaede2d29477015e3d33c1f6a6150
靈夢辛普 <146130238+ReimuSimpu@users.noreply.github.com>
2024-07-19 09:20:26 -0500

    Update Rayfield2.lua

--- Rayfield2.lua
+++ Rayfield2.lua
@@ -881,11 +881,7 @@ end
 function RayfieldLibrary:CreateWindow(Settings)
 	local Passthrough = false
 	Topbar.Title.Text = Settings.Name
-	if game.Players.LocalPlayer.PlayerGui:FindFirstChild("TouchGui") then
-		Main.Size = UDim2.new(0, 0, 0, 0)
-	else
-		Main.Size = UDim2.new(0, 0, 0, 0)
-	end
+	Main.Size = UDim2.new(0, 0, 0, 0)
 	Main.Visible = true
 	Main.BackgroundTransparency = 1
 	Main.Shadow.Image.ImageTransparency = 1
