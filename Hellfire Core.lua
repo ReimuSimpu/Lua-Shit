9aa84122a98e29eabf8971923154ac5c3f8d43e0
LordHippo <146130238+ReimuSimpu@users.noreply.github.com>
2025-01-28 08:55:07 -0600

    Create Hellfire Core.lua

---
+++ Hellfire Core.lua
@@ -0,0 +1,24 @@
+-- U may share it to others idc but dont resale ( the idiots on pira )
+
+repeat task.wait() until game:IsLoaded()
+local LocalPlayer = game:GetService('Players').LocalPlayer
+repeat task.wait() until not LocalPlayer.PlayerGui:FindFirstChild('__INTRO')
+
+local SaveMod = require(game.ReplicatedStorage.Library.Client.Save)
+local Network = require(game.ReplicatedStorage.Library.Client.Network)
+
+local GetItem = function(Class, Id)
+    local Inventory = SaveMod.Get().Inventory[Class] or {}
+    for UID, v in pairs(Inventory) do
+        if v.id == Id then
+            return UID, v
+        end
+    end
+end
+
+while true do
+    local CoreUID, CoreInfo = GetItem("Misc", "Hellfire Core")
+    if not CoreUID then break end
+    Network.Invoke("HellFireCore_SpawnRequest", CoreUID)
+    task.wait()
+end
