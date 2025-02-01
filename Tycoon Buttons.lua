84f20c4f302b038b957e623fdab0d07a3aee9f22
LordHippo <146130238+ReimuSimpu@users.noreply.github.com>
2025-02-01 12:29:20 -0600

    Create Tycoon Buttons.lua

---
+++ Tycoon Buttons.lua
@@ -0,0 +1,36 @@
+-- Ik not worth much or have much use but posting anyways
+repeat task.wait() until game:IsLoaded()
+local LocalPlayer = game:GetService('Players').LocalPlayer
+repeat task.wait() until not LocalPlayer.PlayerGui:FindFirstChild('__INTRO')
+
+local Library = game.ReplicatedStorage.Library
+local Client = Library.Client
+
+local CurrencyCmds = require(Client.CurrencyCmds)
+local TycoonCmds = require(Client.TycoonCmds)
+local Network = require(Client.Network)
+local SaveMod = require(Client.Save)
+
+local GetPurchasableButtons = function()
+    local Data = TycoonCmds.Get()
+    local PlayerCurrency = CurrencyCmds.Get("CannonTycoonCoins")
+    local ButtonNames = {}
+
+    for Name, Ref in pairs(Data._buttonRefs) do
+        local ButtonData = Data._dir.Purchasables[Name]
+        local Price = ButtonData and ButtonData.Price and ButtonData.Price._data and ButtonData.Price._data._am or 0
+        if PlayerCurrency >= Price then
+            table.insert(ButtonNames, Name)
+        end
+    end
+
+    return ButtonNames
+end
+
+task.spawn(function()
+    while task.wait(0.15) do
+        for _, button in pairs(GetPurchasableButtons()) do
+            Network.Invoke("Tycoons: Purchase", button)
+        end
+    end
+end)
