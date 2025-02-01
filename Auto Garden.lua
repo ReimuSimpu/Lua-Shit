f72bf96d9ec644d1fdc87b3be9fc27e137dfb157
LordHippo <146130238+ReimuSimpu@users.noreply.github.com>
2025-02-01 14:15:31 -0600

    Create Auto Garden.lua

---
+++ Auto Garden.lua
@@ -0,0 +1,22 @@
+repeat task.wait() until game:IsLoaded()
+local LocalPlayer = game:GetService('Players').LocalPlayer
+repeat task.wait() until not LocalPlayer.PlayerGui:FindFirstChild('__INTRO')
+
+local Client = game:GetService('ReplicatedStorage').Library.Client
+local OrbCmds = require(Client.OrbCmds.Orb)
+local Network = require(Client.Network)
+
+OrbCmds.CombineDelay, OrbCmds.CollectDistance, OrbCmds.DefaultPickupDistance, OrbCmds.CombineDistance = -math.huge, math.huge, math.huge, math.huge
+
+while task.wait() do
+    if workspace.__THINGS.__INSTANCE_CONTAINER.Active:FindFirstChild("FlowerGarden") then
+        for i = 1, 10 do
+            Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "PlantSeed", i, "Diamond")
+            Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "InstaGrowSeed", i)
+            Network.Invoke("Instancing_InvokeCustomFromClient", "FlowerGarden", "ClaimPlant", i)
+            task.wait()
+        end
+    else
+        setthreadidentity(2) require(Client.InstancingCmds).Enter("FlowerGarden") setthreadidentity(8) task.wait(1)
+    end
+end
