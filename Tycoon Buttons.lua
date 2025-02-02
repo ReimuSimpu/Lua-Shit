19100ecc28b6476d518896d5a2b1d99cf136c60d
LordHippo <146130238+ReimuSimpu@users.noreply.github.com>
2025-02-01 21:39:34 -0600

    Update Tycoon Buttons.lua

--- Tycoon Buttons.lua
+++ Tycoon Buttons.lua
@@ -1,4 +1,5 @@
 -- Ik not worth much or have much use but posting anyways
+
 repeat task.wait() until game:IsLoaded()
 local LocalPlayer = game:GetService('Players').LocalPlayer
 repeat task.wait() until not LocalPlayer.PlayerGui:FindFirstChild('__INTRO')
@@ -16,21 +17,33 @@ local GetPurchasableButtons = function()
     local PlayerCurrency = CurrencyCmds.Get("CannonTycoonCoins")
     local ButtonNames = {}
 
-    for Name, Ref in pairs(Data._buttonRefs) do
-        local ButtonData = Data._dir.Purchasables[Name]
-        local Price = ButtonData and ButtonData.Price and ButtonData.Price._data and ButtonData.Price._data._am or 0
-        if PlayerCurrency >= Price then
-            table.insert(ButtonNames, Name)
+    if Data then
+        for Name, Ref in pairs(Data._buttonRefs) do
+            local ButtonData = Data._dir.Purchasables[Name]
+            local Price = ButtonData and ButtonData.Price and ButtonData.Price._data and ButtonData.Price._data._am or 0
+            if PlayerCurrency >= Price then
+                table.insert(ButtonNames, Name)
+            end
         end
     end
 
     return ButtonNames
 end
 
+-- Button shit
 task.spawn(function()
     while task.wait(0.15) do
         for _, button in pairs(GetPurchasableButtons()) do
             Network.Invoke("Tycoons: Purchase", button)
+        end
+    end
+end)
+
+-- Rebrith
+task.spawn(function()
+    while task.wait(1) do
+        if TycoonCmds.Get()._progressCache >= 1 then
+            Network.Invoke("Tycoons: Rebirth")
         end
     end
 end)
