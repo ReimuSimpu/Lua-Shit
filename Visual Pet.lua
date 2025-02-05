fdbf549a1e54a28cdc2f9994b58b431433516294
LordHippo <146130238+ReimuSimpu@users.noreply.github.com>
2025-02-05 07:11:27 -0600

    Create Visual Pet.lua

---
+++ Visual Pet.lua
@@ -0,0 +1,28 @@
+local Directory = require(game.ReplicatedStorage.Library.Directory)
+
+local ChangeItem = function(Class, FromId, ToId)
+    local Dir = Directory[Class]
+    if not Dir then 
+        return nil, "Invalid Class: " .. Class
+    end
+
+    local FromItem = Dir[FromId]
+    if not FromItem then 
+        return nil, "Invalid From: " .. FromId
+    end
+
+    local ToItem = Dir[ToId]
+    if not ToItem then 
+        return nil, "Invalid To: " .. ToId
+    end
+
+    table.clear(FromItem)
+    for i, v in pairs(ToItem) do
+        FromItem[i] = v
+    end
+
+    return true
+end
+
+local sus, err = ChangeItem("Pets", "Wild Fire Agony", "Huge Hippo")
+print(sus, err)
